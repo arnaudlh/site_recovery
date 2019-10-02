2fe69ca47c240447ec27fd9fc29399ef8c1e5d3c
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-10-02 20:45:41 +0800

    Added module tagging

--- module.tf
+++ module.tf
@@ -11,5 +11,5 @@ resource "azurerm_recovery_services_vault" "asr_rg_vault" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = "Standard"
-  tags                = merge(var.tags, local.tags)
+  tags                = local.tags
 }
